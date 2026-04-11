ed90158acb855622911bccf06110e5557353527a
LongVy-PGStudent <longvy@student.purdueglobal.edu>
2026-04-11 16:46:18 -0400

    Update roles with login and passwords

--- Unit 3/IN450_LongVy_Unit3.sql
+++ Unit 3/IN450_LongVy_Unit3.sql
@@ -21,3 +21,17 @@ GRANT SELECT ON IN450b to IN450b_security;
 
 -- Grant Select to IN450c_security for IN450c table
 GRANT SELECT ON IN450c to IN450c_security;
+
+
+-- GeeksforGeeks. (2025, July 23). How to create login, user and grant permissions in SQL server. https://www.geeksforgeeks.org/sql-server/how-to-create-login-user-and-grant-permissions-in-sql-server/ 
+
+--Create Login credentials
+ALTER ROLE IN450a_security with LOGIN PASSWORD 'password1';
+ALTER ROLE IN450b_security with LOGIN PASSWORD 'password2';
+ALTER ROLE IN450c_security with LOGIN PASSWORD 'password3';
+
+
+
+
+
+
